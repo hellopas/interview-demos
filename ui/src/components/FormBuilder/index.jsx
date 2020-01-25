d6487e5e7c9711e5f6df201e4f2ab80cfd7d3365
Pasindu Wewegama <hello@pasindu.io>
2020-01-25 12:14:27 -0700

    Created form builder title and desc.

---
+++ ui/src/components/FormBuilder/index.jsx
@@ -0,0 +1,46 @@
+/* Import 3rd party libraries */
+import React, { Component } from 'react';
+
+/* Import styles */
+import './FormBuilder.scss';
+
+/* Import my components */
+// import MultipleChoice from 'components/MultipleChoice';
+
+class FormBuilder extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        title: 'Untitled form',
+        description: '',
+    }
+  }
+
+  render() {
+    const { title, description } = this.state;
+
+    return (
+      <div className='formbuilder'>
+        
+        <div className='formbuilder__title'> 
+            <div className='formbuilder__title-header'></div>
+            
+            <div className='formbuilder__title-input'>
+                <input type='text' placeholder='Form title' value={title} onChange={ (evt) => {this.setState({ title: evt.target.value })} }/>
+            </div>
+
+            <div className='formbuilder__title-input formbuilder__description-input'>
+                <input type='text' placeholder='Form description' value={description} onChange={ (evt) => {this.setState({ description: evt.target.value })} }/>
+            </div>
+        </div>
+
+
+      </div>
+
+    );
+  }
+}
+
+export default FormBuilder;
